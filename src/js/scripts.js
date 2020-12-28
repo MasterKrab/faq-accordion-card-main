a4dbb370da52f5f406fe476a6f15bfd5262f6238
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2020-12-27 21:34:57 -0300

    First Commit

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+const faqButton = document.querySelectorAll(`.accordion-faq__question`)
+const faqAnswer = document.querySelectorAll(`.accordion-faq__answers`)
+
+faqButton.forEach((question) => {
+    question.addEventListener(`click`, () =>{
+        if(question.parentNode.classList.contains(`accordion-faq--active`))
+            question.parentNode.classList.remove(`accordion-faq--active`)
+        else{
+            faqButton.forEach((question) => {
+                question.parentNode.classList.remove(`accordion-faq--active`)
+            })
+            question.parentNode.classList.toggle(`accordion-faq--active`)
+        }
+    })
+})
